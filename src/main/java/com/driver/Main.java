d278de7cc2c9eb872cc666b67262b97a5b2a2395
unknown <abiramisaravanansrva@gmail.com>
2023-02-14 14:39:19 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A
+    {
+        public String meth()
+        {
+            return "Involving method from class A";
+        }
+    }
+    public static class B extends A{
+        public String meth(){
+            return "Method is overriding in extended class";
+        }
+    }
+
+    public static void main(String[] args) {
+        B b = new B();
+        b.meth();
+        B b1 = new B();
+        b.meth();
+    }
 }
